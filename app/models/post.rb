086f092d395f80dfa4d94eb1c3653cfe1124db60
AJ Christensen <aj@junglist.gen.nz>
2008-12-30 14:24:05 +1300

    Bundling maruku,syntax,taggable,rake0.8.3

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post
   include DataMapper::Resource
   
   property :id, Serial
+  property :title, String, :nullable => false, :length => 10
+  property :content, Text, :nullable => false, :length => 1000
   has n, :comments
   belongs_to :user
+  is :taggable
 end
